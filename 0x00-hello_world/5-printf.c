63621714567f5f02149bb1e3596b37bded5786fb
Apiyo120 <apiyodianah@gmail.com>
2023-03-17 04:44:14 -0700

    Print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include	<stdio.h>
+/**
+ * main	-	write	the	line	of	command	using	printif
+ *
+ * Return:	0	(Success)
+*/
+int	main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,);
+	return	(0);
+}
