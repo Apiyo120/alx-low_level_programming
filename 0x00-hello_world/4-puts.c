e81f956a3967dea3bb6b677363b7b8543b97c98a
Apiyo120 <apiyodianah@gmail.com>
2023-03-16 17:45:53 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include	<stdio.h>
 /**
- * main	-	this	is	the	main	function
+ * main	prints	the	line	of	code	using	puts
  *
- * Return	is	always	(0)
- */
+ * Return:	is	always	0
+*/
 int	main(void)
-(puts("\"Programming is like building a multilingual puzzle):
-	return	(0):
-)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return	(0);
+}
