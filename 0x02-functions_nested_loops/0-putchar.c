7a29f28f5c7a36b66c6483c1499f9815890df308
Apiyo120 <apiyodianah@gmail.com>
2023-03-21 08:29:19 -0700

    Main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
 #include "main.h"
-
 /**
- * main	-	write -putchar
+ * main	-	print _Putchar
  * written by Apiyo
  * Return: 0 (Success)
 */
-
 int main(void)
 
 {
 	_putchar('_');
-		_putchar('P');
-		_putchar('u');
-		_putchar('t');
-		_putchar('c');
-		_putchar('h');
-		_putchar('a');
-		_putchar('r');
-		_putchar('\n');
-		return (0);
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
