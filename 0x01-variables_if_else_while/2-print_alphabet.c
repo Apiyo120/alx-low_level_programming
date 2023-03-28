4b38f88628036f6c42902a805be41923d2b71aa3
Apiyo120 <apiyodianah@gmail.com>
2023-03-28 11:33:53 -0700

    Aphabets in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main	-	print	letters
+ * main - Prints the alphabet.
  *
- * Return:	0	(Success)
-*/
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-	char letter;
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	for	(letter = 'a' letter <= 'z' letter++)
-
-		putchar("letter");
-
-	putchar("\n " ,);
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
 	return (0);
 }
