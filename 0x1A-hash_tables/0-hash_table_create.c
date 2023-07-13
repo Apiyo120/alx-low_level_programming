d221c54eb4bab76a27345b776908345809be0539
Apiyo120 <apiyodianah@gmail.com>
2023-07-12 18:18:41 -0700

    Hash_tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@
  * Otherwise - print a pointer to the new hash table.
  */
 
-hash_table_t *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 	unsigned long int i;
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size);
 		return (NULL);
 
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size)
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 		if	(ht->array == NULL)
 			return (NULL);
 	for	(i = 0; i < size; i++)
